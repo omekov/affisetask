8cc76e0110ca6bcf1006cd04d2f4b552d326cc38
Azamat Omekov <azamatomekov@gmail.com>
2021-01-21 00:23:56 +0600

    pred podgotovka

--- main.go
+++ main.go
@@ -1,18 +1,67 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
+var (
+	errOnlyMethodsPOSTAndOPTIONS = errors.New("Sorry, only POST and OPTIONS methods are supported")
+	errLimitedURLs               = errors.New("limited urls")
+)
+
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", Router()))
+	ctx, cancel := context.WithCancel(context.Background())
+	httpServer := &http.Server{
+		Addr:        ":8080",
+		Handler:     Router(),
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
+	}
+	go func() {
+		if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+	signalChan := make(chan os.Signal, 1)
+
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,  // kill -SIGHUP XXXX
+		syscall.SIGINT,  // kill -SIGINT XXXX or Ctrl+c
+		syscall.SIGQUIT, // kill -SIGQUIT XXXX
+	)
+
+	<-signalChan
+	log.Print("os.Interrupt - shutting down...\n")
+
+	go func() {
+		<-signalChan
+		log.Fatal("os.Kill - terminating...\n")
+	}()
+
+	gracefullCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelShutdown()
+	if err := httpServer.Shutdown(gracefullCtx); err != nil {
+		log.Printf("shutdown error: %v\n", err)
+		defer os.Exit(1)
+		return
+	} else {
+		log.Printf("gracefully stopped\n")
+	}
+
+	cancel()
+
+	defer os.Exit(0)
+	return
 }
 
 // Router ...
@@ -24,58 +73,59 @@ func Router() *http.ServeMux {
 
 func handlerURL(w http.ResponseWriter, r *http.Request) {
 	var urls []string
-	// receptionMethods := []string{http.MethodOptions, http.MethodPost}
-	// for _, m := range receptionMethods {
-	// 	if r.Method != m {
-	// 		http.Error(w, "", http.StatusMethodNotAllowed)
-	// 		return
-	// 	}
-	// }
-	if r.Method != http.MethodPost {
-		http.Error(w, "", http.StatusMethodNotAllowed)
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Accept", "application/json")
+	switch r.Method {
+	case http.MethodOptions:
+	case http.MethodPost:
+	default:
+		http.Error(w, errOnlyMethodsPOSTAndOPTIONS.Error(), http.StatusMethodNotAllowed)
 		return
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&urls); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	if len(urls) > 20 {
-		http.Error(w, "the number of url is more than 20", http.StatusBadRequest)
+		http.Error(w, errLimitedURLs.Error(), http.StatusBadRequest)
 		return
 	}
 	for _, uri := range urls {
 		u, err := url.Parse(uri)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("parse url err %s", u), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		urlRequest(*u)
+		if err := urlRequest(*u); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
-	fmt.Fprintf(w, "URLs: %+v", urls)
 }
 
 func urlRequest(u url.URL) error {
 	transport := &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout: 5 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 5 * time.Second,
-		MaxConnsPerHost:     200,
+		MaxConnsPerHost: 100,
+		MaxIdleConns:    4,
 	}
 	client := http.Client{
-		Timeout:   10 * time.Second,
+		Timeout:   1 * time.Second,
 		Transport: transport,
 	}
 	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
 		return err
 	}
-	log.Printf("inc %v", req)
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
+	log.Printf("resposne %v", res)
 	if res.StatusCode != http.StatusOK {
 		return errors.New("is not status code 200")
 	}
